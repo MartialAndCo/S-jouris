006071c2170eb48fa4421b1d9b4ddd8ef1cae47e
VISUARA <98740111+visuarafr@users.noreply.github.com>
2025-03-09 15:36:35 +0100

    Déploiement GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,8 @@ try {
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", // Permet d'exporter un site statique
+  distDir: "out",   // Dossier de sortie
   eslint: {
     ignoreDuringBuilds: true,
   },
